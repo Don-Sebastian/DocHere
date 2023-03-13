1f581aa4e670cb6f831bbb50cd7d84e93cb88da0
Don-Sebastian <donsebastian1333@gmail.com>
2023-03-13 10:11:52 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+const PORT = process.env.BACKEND_PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Connected to port", PORT);
+});
