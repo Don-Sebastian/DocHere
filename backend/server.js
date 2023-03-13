60067852f1877ecb1e64fee9c1868d54e2c9cdc2
Don-Sebastian <donsebastian1333@gmail.com>
2023-03-13 10:51:19 +0530

    commit change

--- backend/server.js
+++ backend/server.js
@@ -4,5 +4,5 @@ const app = express();
 
 const PORT = process.env.BACKEND_PORT || 5000;
 app.listen(PORT, () => {
-  console.log("Connected to port", PORT);
+  console.log("Connected to port wit", PORT);
 });
